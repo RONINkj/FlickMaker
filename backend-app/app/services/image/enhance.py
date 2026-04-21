27c367e475533bfc8e5fa0915df666db6181b185
Kanishk Jaiswal <umangkj12@gmail.com>
2026-04-21 13:58:34 +0530

    image enhaceing done here

---
+++ backend-app/app/services/image/enhance.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageEnhance
+import os
+
+
+def enhace_image(input_path: str, output_path: str, brightness: float = 1.1, contrast: float = 1.2, sharpness: float = 1.1) -> str:
+    """
+    Enhances image using basic adjustments.
+    args:
+        input_path (str): Path to input image
+        output_path (str): Path to save enhanced image
+        brightness (float): Brightness factor (default 1.1)
+        contrast (float): Contrast factor (default 1.2)
+        Sharpness (float): Sharpness factor (default 1.1)
+
+    returns:
+        str: Path of enhanced image
+    """
+
+    # open image
+    image = Image.open(input_path).convert("RGB")
+
+    # apply brightness
+    image = ImageEnhance.Brightness(image).enhance(brightness)
+
+    # apply contrast
+    image = ImageEnhance.Contrast(image).enhance(sharpness)
+
+    # Ensure outout directory exists
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # Save enhaced image
+    image.save(output_path)
+
+    return output_path
