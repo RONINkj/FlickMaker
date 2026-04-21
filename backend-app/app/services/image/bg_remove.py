827086352995201e4c7be3725e59965008970d98
Kanishk Jaiswal <umangkj12@gmail.com>
2026-04-21 20:11:35 +0530

    update bg_remove and bg_replace code

--- backend-app/app/services/image/bg_remove.py
+++ backend-app/app/services/image/bg_remove.py
@@ -2,28 +2,17 @@ from rembg import remove
 from PIL import Image
 import os
 
-# take an image --> remove background --> return processed image path
-
-
 def remove_background(input_path: str, output_path: str) -> str:
-    """
-    args:
-        input_path (str): Path to input image
-        output_path (str): Path to save output image (PNG recommended)
-    return:
-        str: Path of processed image
-    """
-
     # open image
     input_image = Image.open(input_path)
 
-    # remove background
-    output_image = remove(input_path)
+    # remove background (pass PIL image, not path)
+    output_image = remove(input_image)
 
-    # ensure output exits in directory
+    # ensure output directory exists
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-    # save output as PNG (imp for transparency)
+    # save output as PNG (important for transparency)
     output_image.save(output_path, format="PNG")
 
     return output_path
--- backend-app/app/services/image/bg_replace.py
+++ backend-app/app/services/image/bg_replace.py
@@ -1,34 +1,23 @@
 from PIL import Image
 import os
 
-# takes an tranparancy -> add new background color -> Final image
-
-
 def apply_background(input_path: str, output_path: str, color: tuple = (255, 255, 255)) -> str:
-    """
-    args: 
-        input_path (str): Path to transparent PNG image (RGBA)
-        output_path (str): Path to save final image.
-        color (tuple): Background color in RGB (default: white)
-    returns:
-        str: Path of processed image
-    """
-    # open image and RGBA
+    # open image and ensure RGBA
     foreground = Image.open(input_path).convert("RGBA")
-    
-    # create background image
+
+    # create solid background
     background = Image.new("RGBA", foreground.size, color + (255,))
-    
-    # compostite foreground onto background
-    combined = Image.alpha_composite(background,foreground)
-    
-    # convert to RGB (remove alpha for further processing)
+
+    # composite images
+    combined = Image.alpha_composite(background, foreground)
+
+    # convert to RGB (remove alpha)
     final_image = combined.convert("RGB")
-    
+
     # ensure output directory exists
-    os.makedirs(os.path.dirname(output_path),exist_ok=True)
-    
-    # save final image
-    final_image.save(output_path)
-    
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # save explicitly
+    final_image.save(output_path, format="JPEG")
+
     return output_path
