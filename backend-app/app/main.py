a4297edc413037b9796ade871d85c9a479803db2
Kanishk Jaiswal <umangkj12@gmail.com>
2026-04-23 13:59:29 +0530

    connection between backend and frontend

--- backend-app/app/main.py
+++ backend-app/app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from app.routes import process
-from fastapi import Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app=FastAPI(title="FlickMaker",version="1.0.0")
 
+origins = ["http://localhost:5173/"]
 
-
+app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=[""],allow_headers=[""])
 
 app.include_router(process.router)
 
